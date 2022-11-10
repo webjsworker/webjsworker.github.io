e4a6a7634791f60db7a4ccf6daa07f97f6f8f2c5
Aliaksei <aliakseiyudziankou@gmail.com>
2022-11-10 23:20:39 +0300

    feat: add contact

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("init")
+/* let elem = document.querySelector('.head') */
+let elem_description = document.querySelector('.main_description')
+let main_contact = document.querySelector('.main_contact')
+main_contact.style.display = 'none'
+
+document.addEventListener('click',e => {
+  
+  let element  = e.target 
+  let element_content = e.target.innerHTML
+  if(element_content == 'About me'){
+    elem_description.style.display = 'block'
+    main_contact.style.display = 'none'
+    console.log(element_content)
+}   
+if(element_content == 'Education'){
+    main_contact.style.display = 'none'
+    elem_description.style.display = 'none'
+    console.log(element_content)
+}
+if(element_content == 'Experience'){
+    
+    main_contact.style.display = 'none'
+    elem_description.style.display = 'none'
+    console.log(element_content)
+}
+if(element_content == 'Skills'){
+  
+    main_contact.style.display = 'none'
+    elem_description.style.display = 'none'
+    console.log(element_content)
+}
+if(element_content == 'Projects'){
+    
+    main_contact.style.display = 'none'
+    elem_description.style.display = 'none'
+    console.log(element_content)
+}
+if(element_content == 'Links'){
+   
+    main_contact.style.display = 'none'
+    elem_description.style.display = 'none'
+    console.log(element_content)
+}
+if(element_content == 'Contact'){
+    main_contact.style.display = 'block'
+    elem_description.style.display = 'none'
+   
+    console.log(element_content)
+}
+
+})
